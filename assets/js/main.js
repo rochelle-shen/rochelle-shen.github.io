b6a7e61813c3ed011f84ebb618c1e8d09683658e
Rochelle Shen <rochelleshen@berkeley.edu>
2017-11-24 23:15:13 -0800

    touched up ui and other info

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,31 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+$(document).ready(function(){
 
+	// hide #back-top first
+	$("#back-top").hide();
+	
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 150) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+
+});
 (function($) {
 
 	skel.breakpoints({
